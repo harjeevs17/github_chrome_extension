de1cd1610b9051546aa5d9a8d1bb148ed732a8a9
Singh <Harjeev.Singh@cerner.com>
2021-09-17 05:43:43 +0530

    made final changes

--- popup.js
+++ popup.js
@@ -1,19 +1,37 @@
 document.addEventListener('DOMContentLoaded',async()=>{
     const button  = document.getElementById('submitBtn');
     const logoutbutton  = document.getElementById('logoutBtn');
+    repoStat = document.getElementById('repoStat');
     form  = document.getElementById('form');
+    prInfoDetails = document.getElementById('prInfoDetails');
+    pullRequestTitle = document.getElementById('pullRequestTitle');
+    stats = document.getElementById('stats');
+    welcomeText = document.getElementById('welcomeText');
     const displayPullCount = async(user)=>{
-        const url = 'https://api.github.com/search/issues?q="'+user+'"state:open%20is:pr';
-        console.log(user)
-        stats = document.getElementById('stats');
-        welcomeText = document.getElementById('welcomeText');
+        const url = 'https://api.github.com/search/issues?q=is:open%20is:pr%20author:'+user+'';
+        const urlRepos = 'https://api.github.com/users/'+user+'/repos';
         try{
             const res = await fetch(url);
+            const resRepos = await fetch(urlRepos);
+
             const data = await res.json();
+            const reposData = await resRepos.json();
+            
+            const number_of_repos = reposData.length;
             const number_of_pr = data.total_count;
-            stats.innerHTML = "Total number of Pull Requests are : "+ number_of_pr;
+            const pr_info = data.items;
+            pr_list = ''
+            for(ele of pr_info){
+                console.log(ele.title)
+                pr_list = pr_list + "<div class='innerDetails'><a target='_blank' href='"+ele.pull_request.html_url+"'><p class='title'>"+ele.title+"</p><p class='state'>"+ele.body+"</p></a></div>"
+            }
+            console.log(pr_list);
+            prInfoDetails.innerHTML = pr_list;
+            stats.innerHTML = "Profile";
+            repoStat.innerHTML = "Repositories:" + number_of_repos
             welcomeText.innerHTML = "Welcome user : " + user
-            sendEmail(number_of_pr);
+            chrome.browserAction.setBadgeText({text:number_of_pr.toString()});
+            //sendEmail(number_of_pr);
             
         }
         catch(err){
@@ -31,7 +49,14 @@ document.addEventListener('DOMContentLoaded',async()=>{
             document.getElementById('logoutForm').setAttribute('style','display: block;');
             document.getElementById('pullCount').setAttribute('style','display: block;');
             console.log("user is " + user)
-            displayPullCount(user.username)    
+            buttonclickhandler(user.username)
+            repoStat.addEventListener("click",function(){
+                console.log("clicked")
+                window.open('https://github.com/'+user.username+'?tab=repositories');
+            })
+            stats.addEventListener("click",function(){
+                window.open('https://github.com/'+user.username);
+            })    
         }
     });
     button.addEventListener("click", function() {
@@ -41,28 +66,108 @@ document.addEventListener('DOMContentLoaded',async()=>{
             document.getElementById('form').setAttribute('style','display: none;');
             document.getElementById('logoutForm').setAttribute('style','display: block;');
         });
+        buttonclickhandler(username);
     });
     logoutbutton.addEventListener("click", function() {
         chrome.storage.local.set({ "username": null }, function(){
             document.getElementById('form').setAttribute('style','display: block;');
             document.getElementById('logoutForm').setAttribute('style','display: none;');
             document.getElementById('pullCount').setAttribute('style','display: none;');
+            document.getElementById('mainText').innerHTML= "GitBot";
+            chrome.browserAction.setBadgeText({text:""});
         });
     });
+    
+    const compareDates = (date)=>{
+        const date1 = new Date(date);
+        const date2 = new Date();
+        const diffTime = Math.abs(date2 - date1);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+        if(diffDays>=7){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    const  buttonclickhandler = (user) => {
+  
+        // Instantiate an new XHR Object
+        const xhr = new XMLHttpRequest();
+        // Open an obejct (GET/POST, PATH,
+        // ASYN-TRUE/FALSE)
+        xhr.open("GET", "https://api.github.com/search/issues?q=is:open%20is:pr%20author:"+user, true);
+        // When response is ready
+        xhr.onload = function () {
+            if (this.status === 200) {
+                // Changing string data into JSON Object
+                data = JSON.parse(this.responseText);
+                const number_of_pr = data.total_count;
+                const pr_info = data.items;
+                stats.innerHTML = "Github Profile";
+                pullRequestTitle.innerHTML = "Pull Requests : "+number_of_pr;
+                pr_list = ''
+                for(ele of pr_info){
+                    if(compareDates(ele.created_at) == 1){
+                        sendEmail(ele);
+                    }
+                    console.log(ele.title)
+                    eleBody = ele.body !=null ? ele.body : "&nbsp;"
+                    pr_list = pr_list + "<div class='innerDetails'><a target='_blank' href='"+ele.pull_request.html_url+"'><p class='title'>"+ele.title+"</p><p class='state'>"+eleBody+"</p></a></div>"
+                }
+                console.log(pr_list);
+                welcomeText.innerHTML =  "Hi ! " + user 
+                prInfoDetails.innerHTML = pr_list;
+                chrome.browserAction.setBadgeText({text:number_of_pr.toString()});
+                document.getElementById('pullCount').setAttribute('style','display: block;');
+                document.getElementById('mainText').innerHTML = "Connected"
+            }
+            else {
+                console.log("File not found");
+            }
+        }
+        // At last send the request
+        xhr.send();
+
+
+        // Instantiate an new XHR Object
+        const xhr2 = new XMLHttpRequest();
+        // Open an obejct (GET/POST, PATH,
+        // ASYN-TRUE/FALSE)
+        xhr2.open("GET","https://api.github.com/users/"+user+"/repos", true);
+        // When response is ready
+        xhr2.onload = function () {
+            if (this.status === 200) {
+                // Changing string data into JSON Object
+                reposData = JSON.parse(this.responseText);
+                const number_of_repos = reposData.length;
+                repoStat.innerHTML = "Repositories:" + number_of_repos
+            }
+            else {
+                console.log("File not found");
+            }
+        }
+        // At last send the request
+        xhr2.send();
+    }
+    
+
 })
-const sendEmail = (count)=>{
+const sendEmail = (data)=>{
     emailjs
       .send(
-        "service_hst09mx",
-        "template_7xvsnlr",
+        "service_nq71ysk",
+        "template_8dfw5yu",
         {
           reply_to: "no_replay@gmail.com",
           to_name: "Harjeev",
           from_name: "Github Bot",
           sender: name,
-          message: "You have got " +count+ " active PR",
+          title: data.title,
+          link: data.pull_request.html_url,
+          date:new Date(data.created_at).toLocaleDateString('en-GB')
         },
-        "user_8fCHR8UMMvdJCNYX8uhVe"
+        "user_KxfUBUoU50GrLsWMSZ7So"
       )
       .then(
         (result) => {
